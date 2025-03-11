6688ad1fbe5e4156ddc9822c95a6f8c469e43dc7
Sunder Kodi <sunder.kodi@paroscale.com>
2025-03-11 18:17:48 +0530

    added options for rescan

--- ctl/internal/cmd/index/rescan.go
+++ ctl/internal/cmd/index/rescan.go
@@ -6,9 +6,13 @@ import (
 	"os/exec"
 
 	"github.com/spf13/cobra"
+	"github.com/thinkparq/beegfs-go/ctl/internal/bflag"
+	"github.com/thinkparq/beegfs-go/ctl/pkg/config"
+	"go.uber.org/zap"
 )
 
 func newGenericRescanCmd() *cobra.Command {
+	var bflagSet *bflag.FlagSet
 	var recurse bool
 	var cmd = &cobra.Command{
 		Args: cobra.MinimumNArgs(1),
@@ -16,10 +20,16 @@ func newGenericRescanCmd() *cobra.Command {
 			if err := checkBeeGFSConfig(); err != nil {
 				return err
 			}
-			return runPythonRescanIndex(args, recurse)
+			return runPythonRescanIndex(args, bflagSet, recurse)
 		},
 	}
-
+	rescanFlags := []bflag.FlagWrapper{
+		bflag.Flag("max-memory", "X", "Max memory usage (e.g. 8GB, 1G)", "-X", ""),
+		bflag.GlobalFlag(config.NumWorkersKey, "-n"),
+		bflag.GlobalFlag(config.DebugKey, "-V=1"),
+		bflag.Flag("no-metadata", "B", "Do not extract BeeGFS specific metadata", "-B", false),
+	}
+	bflagSet = bflag.NewFlagSet(rescanFlags, cmd)
 	cmd.Flags().BoolVar(&recurse, "recurse", false, "Recursively rescan all directories beneath the specified path.")
 
 	return cmd
@@ -34,15 +44,24 @@ func newRescanCmd() *cobra.Command {
 	return s
 }
 
-func runPythonRescanIndex(paths []string, recurse bool) error {
+func runPythonRescanIndex(paths []string, bflagSet *bflag.FlagSet, recurse bool) error {
+	log, _ := config.GetLogger()
+	wrappedArgs := bflagSet.WrappedArgs()
 	for _, path := range paths {
-		allArgs := make([]string, 0, 4)
+		allArgs := make([]string, 0, len(wrappedArgs)+4)
 		allArgs = append(allArgs, createCmd, "-F", path)
+		allArgs = append(allArgs, wrappedArgs...)
 		if recurse {
 			allArgs = append(allArgs, "-U")
 		} else {
 			allArgs = append(allArgs, "-k")
 		}
+		log.Debug("Running BeeGFS Hive Index rescan command",
+			zap.String("path", path),
+			zap.Bool("recurse", recurse),
+			zap.Any("wrappedArgs", wrappedArgs),
+			zap.Any("allArgs", allArgs),
+		)
 		cmd := exec.Command(beeBinary, allArgs...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
@@ -55,7 +74,11 @@ func runPythonRescanIndex(paths []string, recurse bool) error {
 			return fmt.Errorf("error executing index command: %w", err)
 		}
 	}
-	cmd := exec.Command(beeBinary, createCmd, "-S")
+	treeArgs := []string{createCmd, "-S"}
+	log.Debug("Running BeeGFS Hive Index Tree-Summary command",
+		zap.Any("Args", treeArgs),
+	)
+	cmd := exec.Command(beeBinary, treeArgs...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Start()
