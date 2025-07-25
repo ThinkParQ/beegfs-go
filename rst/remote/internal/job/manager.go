2ae102ae0b0bdb56908ee4530d0972099bb0e5b3
Nathan Swartz <112000252+swartzn@users.noreply.github.com>
2025-07-25 09:57:06 -0500

    feat(ctl): TODO merge - improve status results for offloaded files
    Add remote grpc GetStubContents to retrieve the rstId and url from stub file
    Improve offloaded status when --verify-remote is used by checking stub file contents via GetStubContents

--- rst/remote/internal/job/manager.go
+++ rst/remote/internal/job/manager.go
@@ -1160,6 +1160,10 @@ func (m *Manager) GetRSTConfig() ([]*flex.RemoteStorageTarget, error) {
 	return rstConfigs, nil
 }
 
+func (m *Manager) GetStubContents(path string) (uint32, string, error) {
+	return m.workerManager.GetStubContents(m.ctx, path)
+}
+
 func (m *Manager) Stop() {
 
 	m.readyMu.Lock()
